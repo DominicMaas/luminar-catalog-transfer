27820220368c5d85d92f0f12360edfa63404d223
Dominic Maas <dominic.maas@live.com>
2020-12-18 22:26:39 +1300

    Looks like resources are needed and must be moved across as well

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
@@ -58,6 +59,8 @@ foreach (var history in imageHistory)
 Console.WriteLine("Finding matching image ids in destination catalog...");
 
 await using var destinationConnection = new SqliteConnection($"DataSource={destinationPath}");
+await destinationConnection.OpenAsync();
+
 var destinationImages = (await destinationConnection.QueryAsync("SELECT * FROM images")).ToList();
 
 // We no longer care about the ID, it is no longer needed
@@ -71,11 +74,52 @@ foreach (var item in imageHistoryList)
     imageHistory[match._id_int_64] = item;
 }
 
-// Write the edits into the correct table
+await using var writeTransaction = destinationConnection.BeginTransaction();
 
+try
+{
+    // Write the edits into the correct table
+    foreach (var item in imageHistory)
+    {
+        foreach (var history in item.Value.History)
+        {
+            // var currentMaxId = await destinationConnection.QueryFirstAsync<int>("SELECT MAX(_id_int_64) FROM img_history_states");
+            // currentMaxId += 1; // Increment by one for the new max
+        
+            // Insert a history state
+            var insertedId = await destinationConnection.QueryFirstAsync<long>(
+                "INSERT INTO img_history_states (marked_to_delete_bool, name_wide_ch, data_wide_ch, guid_wide_ch, crop_info_wide_ch, image_orientation_int_64, hash_wide_ch, original_bool) VALUES(@marked_to_delete_bool, @name_wide_ch, @data_wide_ch, @guid_wide_ch, @crop_info_wide_ch, @image_orientation_int_64, @hash_wide_ch, @original_bool); SELECT last_insert_rowid()",
+                new
+                {
+                    history.marked_to_delete_bool, 
+                    history.name_wide_ch, 
+                    history.data_wide_ch, 
+                    history.guid_wide_ch, 
+                    history.crop_info_wide_ch, 
+                    history.image_orientation_int_64, 
+                    history.hash_wide_ch, 
+                    history.original_bool
+                }, commandType: CommandType.Text, transaction: writeTransaction);
+        
+            // Insert a proxy
+            await destinationConnection.ExecuteAsync(
+                "INSERT INTO image_history_state_proxy (_image_id_int_64, _state_id_int_64, current) VALUES(@ImageId, @StateId, @Current)",
+                new
+                {
+                    ImageId = item.Key,
+                    StateId = insertedId,
+                    Current = (history._id_int_64 == item.Value.CurrentId) ? 1 : 0
+                }, commandType: CommandType.Text, transaction: writeTransaction);
+        }
+    }
 
-
-Console.WriteLine("Hello World!");
+    await writeTransaction.CommitAsync();
+}
+catch (Exception e)
+{
+    await writeTransaction.RollbackAsync();
+    Console.WriteLine("Something went wrong! Rolling back changes: " + e.Message);
+}
 
 class ImageHistory
 {
