28fb5f14bae9efd1e967f875c76a8c10ead61e1d
Dominic Maas <dominic.maas@live.com>
2020-12-18 19:57:31 +1300

    Add initial files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuminarCatalogTransfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
